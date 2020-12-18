2481271fe4f13a6fe4c8ba8415c07b6588b765e6
NathanDeodato <ndeodato42@outlook.com>
2020-12-18 17:51:48 -0300

    Added folder Jupy-v1

---
+++ Jupy-v1/jupy-v1.py
@@ -0,0 +1,203 @@
+print("-=" * 50)
+print("-- Jupy$ v1 --".center(100))
+print("-=" * 50)
+
+print("Óla! Me chamo Jupy.")
+print("Fui criado, com o objetivo de responder ao usúario,")
+print("ou seja, você. Perguntas as quais tenha a em meu BD(Banco de dados).")
+print("Espero conseguir te ajudar.")
+
+print("-=" * 50)
+
+def jupyv1():
+    # Libs
+    from time import sleep
+
+    # Comands Jupy
+
+
+    # Pergunta do usúario
+    print("Jupy$: Começe a pergunta com 'o que'")
+    pergunta = str(input("Pergunta$: ")).lower()
+
+    print("-=" * 50)
+
+    jupy = "Jupy$"
+
+    print("Processando...")
+    
+    ## Time
+    sleep(2)
+
+    # Banco de dados de respostas
+
+    def breaklin():
+        print("")
+
+    ## NT 
+    if "Key Master: xbox360N" in pergunta:
+        pass
+
+    ## Lista de conhecimento do Jupy
+    if pergunta == "Jupy list":
+        print("Jupy$: ...")
+        print("Hardware")
+        print("Linguagem de programação")
+        print("Empresas de tecnologia")
+        print("Consoles")
+
+    ## Componentes eletrônicos
+    if pergunta == "o que e cpu?" or pergunta == "o que e cpu" or pergunta == "o que e uma cpu?" or pergunta == "o que e uma cpu" or pergunta == "o que significa cpu?" or pergunta == "o que significa cpu":
+        print("Jupy$: CPU é a sigla para Central Process Unit, ou Unidade Central de Processamento.")
+        print("Ele é o principal item de hardware do computador, que também é conhecido como processador.")
+        print("A CPU é responsável por calcular e realizar tarefas determinadas pelo usuário e é considerado o cérebro do PC.")
+
+    elif pergunta == "o que e gpu?" or pergunta == "o que e gpu" or pergunta == "o que e uma gpu?" or pergunta == "o que e uma gpu" or pergunta == "o que significa gpu?" or pergunta == "o que significa gpu":
+        print("Jupy$: GPU (Graphics Processing Unit, ou Unidade de Processamento Gráfico), conhecido também")
+        print("como VPU ou unidade de processamento visual, é um tipo de microprocessador especializado em processar gráficos")
+        print("em computadores pessoais, estações de trabalho ou videogames. GPUs modernas manipulam gráficos")
+        print("computadorizados com eficiência e sua estrutura de processamento paralelo os tornam mais capazes neste")
+        print("tipo de trabalho que CPUs normais. Uma GPU normalmente é utilizada em placas de vídeo")
+        print("(este é chamado de Vídeo Offboard ou Placa de Vídeo Dedicada), mas versões simplificadas são integradas diretamente")
+        print("na placas-mãe o que é chamado de Acelerador Gráfico Integrado ou Placa de Vídeo Onboard.")
+
+    elif pergunta == "o que e memoria ram?" or pergunta == "o que e memoria ram" or pergunta == "o que e uma memoria ram?" or pergunta == "o que e uma memoria ram" or pergunta == "o que significa memoria ram?" or pergunta == "o que significa memoria ram":
+        print("Jupy$: A Memória de acesso randômico (português brasileiro) ou Memória de acesso aleatório (português europeu)")
+        print("(do inglês Random Access Memory, frequentemente abreviado para RAM) é um tipo de memória")
+        print("que permite a leitura e a escrita, utilizada como memória primária em sistemas eletrônicos digitais.")
+
+        breaklin()
+
+        print("Jupy$: A RAM é um componente essencial não apenas nos computadores pessoais, mas em qualquer")
+        print("tipo de computador, pois é onde basicamente ficam armazenados os programas básicos operacionais.")
+        print("Por mais que exista espaço de armazenamento disponível, na forma de um HDD ou memória flash, é sempre necessária")
+        print("uma certa quantidade de RAM.")
+
+    elif pergunta == "o que e placa mãe?" or pergunta == "o que e placa mãe" or pergunta == "o que e uma placa mãe?" or pergunta == "o que e uma placa mãe" or pergunta == "o que significa placa mãe?" or pergunta == "o que significa placa mãe":
+        print("Jupy$: A placa-mãe (do inglês motherboard, composto de mother 'mãe' e board 'placa'; também")
+        print("chamada em inglês de mainboard) é a parte do computador responsável por conectar e interligar todos os")
+        print("componentes, ou seja, processador com memória RAM, disco rígido, placa gráfica, entre outros. Além de permitir o")
+        print("tráfego de informação, a placa também alimenta alguns periféricos com a energia elétrica que recebe da fonte de alimentação.")
+
+    elif pergunta == "o que e fonte?" or pergunta == "o que e fonte" or pergunta == "o que e uma fonte?" or pergunta == "o que e uma fonte" or pergunta == "o que significa fonte?" or pergunta == "o que significa fonte":
+        print("Jupy$: Uma fonte de alimentação é um equipamento usado para alimentar cargas elétricas.")
+        print("Cada dispositivo eletroeletrônico necessita de uma fonte para prover energia para seus componentes.")
+        print("Esta energia pode variar de acordo com a carga que este equipamento usa.")
+        print("Estas fontes de energia podem ser de corrente contínua como um conversor AC/DC ou um")
+        print("regulador de tensão, pode ser um regulador linear, fonte de energia AC, fonte de alimentação")
+        print("ininterrupta ou fonte de energia de alta tensão.")
+
+    elif pergunta == "o que e gabinete?" or pergunta == "o que e gabinete" or pergunta == "o que e uma gabinete?" or pergunta == "o que e um gabinete":
+        print("Jupy$: Um gabinete de computador, também conhecido como case, caixa, chassis, carcaça ou torre, é o")
+        print("compartimento que contém a maioria dos componentes de um computador (normalmente, excluindo o monitor, teclado e mouse).")
+        print("Um case de computador, às vezes, é referido metonimicamente como CPU, referindo-se a um componente situado dentro da caixa.")
+        print("CPU era um termo comum nos primeiros computadores domésticos, quando outros periféricos da placa-mãe")
+        print("normalmente eram alojados em seus próprios cases separados.")
+
+        breaklin()
+
+        print("Jupy$: Cases, geralmente, são construídos em aço (muitas vezes, SECC — aço eletrogalvanizado, laminado a frio, e bobina) ou alumínio.")
+        print("Plástico é, por vezes, utilizado, e outros materiais, como madeira aparecem em cases construídos em casa.")
+
+    ## Empresas de Tecnologia
+    if pergunta == "":
+        pass
+
+    ## Consoles
+    if pergunta == "o que é um xbox 360?" or pergunta == "o que é um xbox 360" or pergunta == "o que é o xbox 360?" or pergunta == "o que é o xbox 360":
+        print("Jupy$: O Xbox 360 é um console de video games desenvolvido pela Microsoft. Como sucessor do Xbox original, é o segundo console da série Xbox.")
+        print("Ele competiu com o PlayStation 3 da Sony e o Wii da Nintendo como parte da sétima geração de consoles.")
+        print("Foi oficialmente anunciado na MTV em 12 de maio de 2005, com lançamento detalhado e informações dos jogos anunciadas mais tarde na edição de 2005 da E3.")
+
+        print("Jupy$: O Xbox 360 possui um serviço online, a Xbox Live, que foi expandida a partir de sua iteração anterior no Xbox original")
+        print("e recebeu atualizações regulares durante a vida útil do console.")
+        print("Disponível em variedades gratuitas e baseadas em assinatura, a Xbox Live permite aos usuários jogar jogos online; baixar jogos (através da Xbox Live Arcade)")
+        print("e demos; comprar e transmitir músicas, programas de televisão e filmes através dos portais Xbox Music e Xbox Video e acessar serviços de")
+        print("conteúdo de terceiros através de aplicativos de transmissão de mídia. Além dos recursos multimídia on-line, ele permite aos usuários transmitir mídia de PCs locais.")
+        print("Vários periféricos foram lançados, incluindo controles sem fio, discos rígidos com armazenamento expandido e a câmera sensora de movimentos: o Kinect.")
+        print("O lançamento desses serviços adicionais e periféricos ajudou a marca Xbox a crescer a partir de jogos para englobar todos os")
+        print("multimídia, transformando-o em um hub para entretenimento na sala de estar.")
+
+    elif pergunta == "o que é um xbox one?" or pergunta == "o que é um xbox one" or pergunta == "o que é o xbox one?" or pergunta == "o que é o xbox one":
+        print("Jupy$: Xbox One é um console de videojogos, da oitava geração, produzida pela empresa Microsoft, lançado em 2013, como a terceira edição da série Xbox")
+        print("e, sucessor do Xbox 360. Competindo diretamente com os consoles PlayStation 4 e Nintendo Switch. Introduzido no mercado oito anos")
+        print("após o lançamento do Xbox 360. O Xbox One apresenta jogos com gráficos de alta definição superiores aos vistos no seu antecessor.")
+        print("Foi anunciado no dia 21 de maio de 2013, apresentado ao público pelo presidente de negócios de entretenimento interativo da Microsoft, Don Mattrick,")
+        print("no evento especial Xbox Reveal, como o sucessor do Xbox 360. Seu lançamento oficial foi feito em novembro de 2013 custando US$ 499,00 (€ 499,00 ou R$ 2.299,00).")
+        print("O codinome de desenvolvimento do Xbox One foi denominado de Durango e foi anunciado oficialmente no dia 21 de maio de 2013.")
+
+    elif pergunta == "o que é um xbox series x?" or pergunta == "o que é um xbox series x" or pergunta == "o que é o xbox series x?" or pergunta == "o que é o xbox series x":
+        print("Jupy$: O Xbox Series X e o Series S (coletivamente chamados de Xbox Series X/S[a]) são consoles domésticos de jogos eletrônicos desenvolvidos pela Microsoft.")
+        print("É a quarta geração da família de consoles Xbox; foi anunciada pela primeira vez durante a E3 2019 como Project Scarlett.")
+        print("Ambos os consoles foram lançados em 10 de novembro de 2020.")
+
+        print("Jupy$: Ambos os consoles sucedem a linha atual do Xbox One, substituindo o carro-chefe do Xbox One X e os modelos de baixo custo do Xbox One S, respectivamente.")
+        print("A Microsoft está priorizando o desempenho do hardware, incluindo suporte para resoluções de tela mais altas (até 8K de resolução)")
+        print("e taxas de quadros, ray tracing em tempo real e uso de unidade de estado sólido de alta velocidade para reduzir o tempo de carregamento.")
+        print("O Xbox Series S usa a mesma CPU e GPU, memória e armazenamento interno reduzidos e não possui uma unidade óptica.")
+
+        print("Jupy$: O Xbox Series X é alimentado por uma CPU AMD Zen 2 customizada de 7 nm com oito núcleos rodando a 3,8 GHz nominal ou quando é usado em")
+        print("multithreading simultâneo (SMT), a 3,6 GHz. Um núcleo da CPU é dedicado ao sistema operacional subjacente.")
+        print("A unidade de processamento gráfico também é uma unidade baseada na arquitetura gráfica RDNA 2 da AMD.")
+        print("Ele tem um total de 56 unidades computacionais (CUs) com 3584 núcleos, com 52 CUs e 3328 núcleos ativados e estará rodando a uma velocidade fixa de 1.825 GHz.")
+        print("Esta unidade é capaz de 12.155 teraflops de potência computacional. A unidade é fornecida com 16 GB de SDRAM GDDR6,")
+        print("com 10 GB rodando a 560 GB/s para ser usado principalmente com o sistema gráfico e os outros 6 GB a 336 GB/s para as outras funções de computação.")
+        print("Após contabilizar o software de sistema, aproximadamente 13,5 GB de memória estarão disponíveis para jogos e outros aplicativos, com o software de sistema apenas usando recursos do pool mais lento.")
+
+        print("Jupy$: A performance alvo do Xbox Series X é renderizar jogos em resolução 4K a 60 quadros por segundo. A Microsoft afirmou que a CPU do console será quatro vezes mais")
+        print("poderosa que o Xbox One X, incluindo suporte à ray tracing em tempo real, renderização de até 120 quadros por segundo e resolução 8K através do padrão HDMI 2.1.")
+        print("O console também suporta novos recursos do padrão HDMI 2.1, incluindo taxa de atualização variável (VRR) e Modo Automático de")
+        print("Baixa Latência (ALLM), que estão sendo incorporados às televisões mais recentes.[9] O console terá aceleração de hardware de áudio dedicada.")
+        print("Um recurso chamado 'audio ray tracing' usará os processadores gráficos de ray tracing para processar o áudio espacial da mesma maneira para melhorar a imersão de áudio para o jogador.")
+
+    elif pergunta == "o que é um xbox series s?" or pergunta == "o que é um xbox series s" or pergunta == "o que é o xbox series s?" or pergunta == "o que é o xbox series s":
+        print("Jupy$: ")
+
+    elif pergunta == "o que é um ps3?" or pergunta == "o que é um ps3" or pergunta == "o que é o ps3?" or pergunta == "o que é o ps3":
+        print("Jupy$: ")
+
+    elif pergunta == "o que é um ps4?" or pergunta == "o que é um ps4" or pergunta == "o que é o ps4?" or pergunta == "o que é o ps4":
+        print("Jupy$: ")
+
+    elif pergunta == "o que é um ps5?" or pergunta == "o que é um ps5" or pergunta == "o que é o ps5?" or pergunta == "o que é o ps5":
+        print("Jupy$: ")
+
+
+    ## Se não tiver nenhuma informação sobre a pergunta
+    else:
+        pergunta_erro = pergunta.split()
+        print(f"Jupy$: Desculpa, mas não tenho nenhuma informação sobre '{pergunta_erro[-1]}' em meu banco de dados.")
+
+    ## Time
+    sleep(2)
+
+    print("-=" * 50)
+
+
+# Loop do Jupy
+while True:
+    from time import sleep
+    ## Time
+    sleep(2)
+    
+    jupyv1()
+
+    print("-- Opções --".center(100))
+    print("[ Azul ] Novamente")
+    print("[ Vermelho ] Sair")
+
+    print("-=" * 50)
+
+    user = str(input("Opção$: ")).capitalize()
+
+    print("-=" * 50)
+
+    if user == "Azul":
+        jupyv1()
+    elif user == "Vermelho":
+        print("-=" * 50)
+        print("Jupy$: Encerrando...".center(100))
+        print("-=" * 50)
+        break
+    else:
+        pass
