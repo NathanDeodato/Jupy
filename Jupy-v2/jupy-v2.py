350e8bc84d394910450425818a6231cbf2f12dd1
NathanDeodato <ndeodato42@outlook.com>
2020-12-22 18:31:40 -0300

    Update Jupy-v2

--- Jupy-v2/jupy-v2.py
+++ Jupy-v2/jupy-v2.py
@@ -6,13 +6,17 @@ def winJupy():
     sg.theme("DarkGrey11")
 
     layout = [
-        [sg.Text("Jupy$ - ")],
-        [sg.Text("Pergunta", size=(9, 0)), sg.Input(size=(30, 0), key="pergunta")],
-        [sg.Text(" " * 60), sg.Button("OK", size=(4, 0))],
+        [sg.Text("Jupy$ - Óla! Me chamo Jupy. Fui criado com o objetivo de responder")],
+        [sg.Text("ao usúario. Tenho respostas registradas em meu Banco de dados.")],
+        [sg.Text("Jupy$ - Tenho informações limitadas, mas espero conseguir ajudar você")],
 
-        [sg.Output(size=(60, 10))]
+        [sg.Text("Pergunta$:", size=(9, 0)), sg.Input(size=(51, 0), key="pergunta")],
+        [sg.Text("=" * 22), sg.Button("OK", size=(4, 0)), sg.Text("=" * 22)],
+
+        [sg.Output(size=(60, 12))],
+        [sg.Text(" " * 38), sg.Text("(C) NT Developer")]
     ]
-    return sg.Window("Jupy$", layout=layout)
+    return sg.Window("Jupy$-v2", layout=layout)
 
 
 window = winJupy()
@@ -24,6 +28,8 @@ while True:
     if event == sg.WIN_CLOSED:
         break
 
-    
+    # Pergunta
+    perg = str(values["pergunta"])
 
-
+    if event == "OK":
+        pass
