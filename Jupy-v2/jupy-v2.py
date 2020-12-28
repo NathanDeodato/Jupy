44f2704979da0853f0b654235d613581806eb49a
NathanDeodato <ndeodato42@outlook.com>
2020-12-28 09:30:17 -0300

    Update Jupy-v2

--- Jupy-v2/jupy-v2.py
+++ Jupy-v2/jupy-v2.py
@@ -10,11 +10,17 @@ def winJupy():
         [sg.Text("ao usúario. Tenho respostas registradas em meu Banco de dados.")],
         [sg.Text("Jupy$ - Tenho informações limitadas, mas espero conseguir ajudar você")],
 
-        [sg.Text("Pergunta$:", size=(9, 0)), sg.Input(size=(51, 0), key="pergunta")],
-        [sg.Text("=" * 22), sg.Button("OK", size=(4, 0)), sg.Text("=" * 22)],
+        [sg.Text("_" * 68)],
 
-        [sg.Output(size=(60, 12))],
-        [sg.Text(" " * 38), sg.Text("(C) NT Developer")]
+        [sg.Text(" " * 50), sg.Text("-- OPÇÕES --")],
+        [sg.Radio("Comando Jupy", group_id="opc"), sg.Radio("Hardware", group_id="opc"), sg.Radio("Linguagem de Programção", group_id="opc"), sg.Radio("Console", group_id="opc")],
+        
+        [sg.Text("_" * 68)],
+
+        [sg.Text("Pergunta$:", size=(9, 0)), sg.Input(size=(41, 0), key="pergunta"), sg.Button("Send", size=(5, 0))],
+
+        [sg.Output(size=(66, 12))],
+        [sg.Text(" " * 45), sg.Text("(C) NT Developer")]
     ]
     return sg.Window("Jupy$-v2", layout=layout)
 
